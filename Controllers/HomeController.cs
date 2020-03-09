d7e2166aa5c64346c74186e4d3eac4d45631fbbf
Dharmendra Puttu <dharmendraputtu@gmail.com>
2020-03-09 17:15:40 +0530

    y56y

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SampleWebsite.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "I like the page page3455.";
+            ViewBag.Message = "I l page page3455.";
 
             return View();
         }
